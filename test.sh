78f751edd894c68007f30a3bebda8be6d4062559
Justin Dray <justin@dray.be>
2020-01-30 20:43:23 +1000

    Add xml coverage report for codecov.io

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-poetry run pytest --cov-report term-missing --cov-report html --cov convert_videos tests/
+poetry run pytest --cov-report term-missing --cov-report xml --cov-report html --cov convert_videos tests/
