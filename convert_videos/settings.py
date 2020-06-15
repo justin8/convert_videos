bfe31e62798b095455ae8acdf7c4f84925ba0617
Justin Dray <justin@dray.be>
2020-06-15 22:05:14 +1000

    Don't fail when there are no subtitles on the source video

--- convert_videos/settings.py
+++ convert_videos/settings.py
@@ -47,10 +47,13 @@ class VideoSettings:
 
     def _get_quality_settings(self):
         if self.hw_accel == "nvidia":
-            # nvenc doesn't support CRF; only CQ and QP modes. QP consistently provides better quality output for the same bitrate on NVENC however, so we're using that
+            # nvenc doesn't support CRF; only CQ and QP modes.
+            # QP consistently provides better quality output for the same bitrate on NVENC however, so we're using that
             output = f" -rc constqp -qp {self.quality}"
             output += " -b:v 0"  # Unless bitrate is set to 0, CQ is mostly ignored
-            # output += f" -rc-lookahead -b_ref_mode middle" # b-frames are disabled by default, and only supported on Turing+ architectures. Need to auto-detect this
+
+            # b-frames are disabled by default, and only supported on Turing+ architectures. Need to auto-detect this
+            # output += f" -rc-lookahead -b_ref_mode middle"
         else:
             output = f" -crf {self.quality}"
         return output
@@ -63,7 +66,7 @@ class VideoSettings:
 
     def _get_stream_settings(self):
         output = " -map 0:v:0"  # Include first video stream
-        output += " -map 0:s"  # Include all subtitle streams
+        output += " -map 0:s?"  # Include all subtitle streams, if they exist
         output += " -c:s copy"
         return output
 
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -9,26 +9,22 @@ def test_audio_settings():
 
 def test_video_settings_avc():
     target = VideoSettings(Codec("AVC"), 25, "slow", None)
-    assert str(
-        target) == " -map 0:v:0 -map 0:s -c:s copy -vcodec h264 -preset slow -crf 25"
+    assert str(target) == " -map 0:v:0 -map 0:s? -c:s copy -vcodec h264 -preset slow -crf 25"
 
 
 def test_video_settings_avc_nvidia():
     target = VideoSettings(Codec("AVC"), 25, "slow", hw_accel="nvidia")
-    assert str(
-        target) == " -map 0:v:0 -map 0:s -c:s copy -vcodec h264_nvenc -preset slow -rc constqp -qp 25 -b:v 0"
+    assert str(target) == " -map 0:v:0 -map 0:s? -c:s copy -vcodec h264_nvenc -preset slow -rc constqp -qp 25 -b:v 0"
 
 
 def test_video_settings_hevc():
     target = VideoSettings(Codec("HEVC"), 25, "slow", None)
-    assert str(
-        target) == " -map 0:v:0 -map 0:s -c:s copy -vcodec libx265 -preset slow -crf 25 -strict -2"
+    assert str(target) == " -map 0:v:0 -map 0:s? -c:s copy -vcodec libx265 -preset slow -crf 25 -strict -2"
 
 
 def test_video_settings_hevc_nvidia():
     target = VideoSettings(Codec("HEVC"), 25, "slow", hw_accel="nvidia")
-    assert str(
-        target) == " -map 0:v:0 -map 0:s -c:s copy -vcodec hevc_nvenc -preset slow -rc constqp -qp 25 -b:v 0 -strict -2"
+    assert str(target) == " -map 0:v:0 -map 0:s? -c:s copy -vcodec hevc_nvenc -preset slow -rc constqp -qp 25 -b:v 0 -strict -2"
 
 
 def test_video_settings_width():
@@ -38,5 +34,4 @@ def test_video_settings_width():
         preset="slow",
         width=720,
         hw_accel=None)
-    assert str(
-        target) == " -map 0:v:0 -map 0:s -c:s copy -vcodec h264 -preset slow -crf 25 -vf scale=720:-2"
+    assert str(target) == " -map 0:v:0 -map 0:s? -c:s copy -vcodec h264 -preset slow -crf 25 -vf scale=720:-2"
